8bbc5f7cc4a597e4d739e0f6795a960e025c9ea5
Himadri Talukder <himadricuet@gmail.com>
2019-03-15 13:13:28 -0400

    configured entrypoint

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.war" "$@"
+cp task-application-0.0.1-SNAPSHOT.war app.war
+exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "app.war" "$@"
